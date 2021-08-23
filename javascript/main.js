d8a7d5aa6215d53d3c2b8c5459f86265c52040e2
kmofori <>
2021-08-23 11:58:13 +0100

    updated site, added details to projects

--- javascript/main.js
+++ javascript/main.js
@@ -3,6 +3,17 @@ const navMenu = document.getElementById('nav-menu');
 const toggleMenu = document.getElementById('nav-toggle');
 const closeMenu = document.getElementById('nav-close');
 
+// Modal Constants
+const kalawuButton = document.getElementById('view-kalawu');
+const kalawuModal = document.getElementById('kalawu-modal');
+const duksButton = document.getElementById('view-duks');
+const duksModal = document.getElementById('duks-modal');
+const deciderButton = document.getElementById('view-decider');
+const deciderModal = document.getElementById('decider-modal');
+const chakulaButton = document.getElementById('view-chakula');
+const chakulaModal = document.getElementById('chakula-modal');
+const modalClose = document.getElementById('modal-close');
+
 // Show Navbar
 toggleMenu.addEventListener('click', () => {
   navMenu.classList.toggle('show');
@@ -50,3 +61,45 @@ function scrollAtive() {
     }
   });
 }
+
+// Make modal Visible
+kalawuButton.addEventListener('click', function () {
+  kalawuModal.classList.add('modal-active');
+});
+duksButton.addEventListener('click', function () {
+  duksModal.classList.add('modal-active');
+});
+deciderButton.addEventListener('click', function () {
+  deciderModal.classList.add('modal-active');
+});
+chakulaButton.addEventListener('click', function () {
+  chakulaModal.classList.add('modal-active');
+});
+
+// Close modal by clicking on close button
+modalClose.addEventListener('click', function () {
+  kalawuModal.classList.remove('modal-active');
+});
+modalClose.addEventListener('click', function () {
+  duksModal.classList.remove('modal-active');
+});
+modalClose.addEventListener('click', function () {
+  deciderModal.classList.remove('modal-active');
+});
+modalClose.addEventListener('click', function () {
+  chakulaModal.classList.remove('modal-active');
+});
+
+// close modal by clicking on background
+kalawuModal.addEventListener('click', function () {
+  kalawuModal.classList.remove('modal-active');
+});
+duksModal.addEventListener('click', function () {
+  duksModal.classList.remove('modal-active');
+});
+deciderModal.addEventListener('click', function () {
+  deciderModal.classList.remove('modal-active');
+});
+chakulaModal.addEventListener('click', function () {
+  chakulaModal.classList.remove('modal-active');
+});
